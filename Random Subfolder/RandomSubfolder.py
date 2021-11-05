6965f82e316bf500be7f5bcd3ba10b9bf67e02c0
weiyang2048 <weiyang2048@gmail.com>
2021-11-04 21:04:32 -0400

    Update RandomSubfolder.py

--- Random Subfolder/RandomSubfolder.py
+++ Random Subfolder/RandomSubfolder.py
@@ -1,4 +1,7 @@
 import os 
+import random
+folders = [x[0]  for x in os.walk(r"C:\Users\WEIYA\Google Drive") 
+            if os.path.isdir(x[0])==True 
+                and all(t not in x[0] for t in [".git",".tmp", "ipynb"])] 
 
-folders = [x[0]  for x in os.walk(r"C:\Users\WEIYA\Google Drive") if os.path.isdir(x[0])==True and ".tmp" not in x[0]] 
-os.startfile(folders[1])
+os.startfile(random.choice(folders))
